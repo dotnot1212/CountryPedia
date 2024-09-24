0abd1fe8bbfa1a5de3fce8366ab65b79586d5131
dotnot1212 <157882933+dotnot1212@users.noreply.github.com>
2024-09-24 17:32:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,23 @@ let modeBtn = document.querySelector(".dark-mode .box button");
 let cardSubHead = document.querySelectorAll(".card-shadow");
 let modeFlag = localStorage.getItem("modeFlag") || "light";
 let boxElement = document.querySelector(".dark-mode .box");
+let form = document.querySelector(".country-input");
+let countryInput = document.querySelector(".country-input input");
+let listElem = document.querySelector(".country-list");
+const backToTopButton = document.getElementById("back-to-top");
 
+let countryList = [];
 // filter variable
 let filterItems = document.querySelectorAll(
   ".filter-line .list .item-filter i"
 );
+
+// load Event in browser
 window.addEventListener("load", () => {
   setModeSetting();
-  countNum();
 });
 // events
+
 modeBtn.addEventListener("click", changeMode);
 filterItems.forEach((elem) => {
   console.log(elem);
@@ -25,8 +32,36 @@ filterItems.forEach((elem) => {
   );
 });
 
+// disable preventDefalut
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// event for keyup in keyboard
+countryInput.addEventListener("keyup", searchCountry);
+
+// search input Value
+function searchCountry(e) {
+  if (e.keyCode == 13) {
+    countryFindRun(e.key);
+  }
+  countryFindRun(e.key);
+}
+
 // functions ...
 
+// find country of user want to see
+function countryFindRun() {
+  const foundCountry = countryList.filter((country) => {
+    let inputValue = countryInput.value.toLowerCase().trim();
+    return country.name.toLowerCase().includes(inputValue);
+  });
+
+  listElem.innerHTML = "";
+  foundCountry.forEach((countryFounded) => {
+    countriesGen(countryFounded);
+  });
+}
 // change mode
 function changeMode() {
   if (modeFlag === "dark") {
@@ -61,6 +96,7 @@ function setModeSetting() {
     });
   }
 }
+
 // counter number
 function countNum() {
   let allNum = document.querySelectorAll(".card-shadow .card-text");
@@ -85,3 +121,111 @@ function countNum() {
   });
 }
 
+const noticeElems = document.querySelectorAll(".notice");
+const infoSIde = document.querySelectorAll(".side-information");
+
+function countriesGen(country) {
+  listElem.insertAdjacentHTML(
+    "beforeend",
+    ` <div class="col-12 my-2">
+                <div
+                  class="country-box p-3 rounded-3 d-flex flex-column flex-md-row align-items-center"
+                >
+                  <div
+                    class="col-12 col-md-1 d-flex justify-content-center align-items-center"
+                  >
+                    <img
+                      class="country-img d-flex mb-3 mb-md-0"
+                      src="${country.flag}"
+                      alt=""
+                    />
+                  </div>
+                  <div class="col-12 col-md-11">
+                    <ul class="list mb-0 list-unstyled d-flex">
+                      <li class="item">
+                        نام : <span class="name-item">${country.name}</span>
+                      </li>
+                      <li class="item">
+                        قاره : <span class="continent-item">${country.continent}</span>
+                      </li>
+                      <li class="item">
+                        مساحت : <span class="size-item">(km2) ${country.area} </span>
+                      </li>
+                      <li class="item">
+                        جهت رانندگی : <span class="driving-item">${country.drivingSide}</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>`
+  );
+  let countNumExecuted = false;
+
+  const countryBoxes = document.querySelectorAll(".country-box");
+  window.addEventListener("scroll", () => {
+    countryBoxes.forEach((box) => {
+      const rect = box.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        box.classList.add("in-view");
+      } else {
+        box.classList.remove("in-view");
+      }
+    });
+
+    noticeElems.forEach((note) => {
+      const rect = note.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        note.classList.add("in-view");
+        document.querySelector(".section-borders").classList.add("in-view");
+        if (!countNumExecuted) {
+          countNum(); // Call countNum() only if it hasn't been executed before
+          countNumExecuted = true; // Set the flag to true
+        }
+      }
+    });
+
+    infoSIde.forEach((info) => {
+      const rect = info.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        info.classList.add("in-view");
+      }
+    });
+
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 200) {
+      backToTopButton.classList.remove('d-none')
+    } else {
+      backToTopButton.classList.add('d-none')
+
+    }
+
+
+  });
+}
+
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+// api countries
+const url = `https://restcountries.com/v3.1/all`;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    let countries = data;
+    console.log(typeof data);
+    for (const country of countries) {
+      let infoCountry = {
+        name: `${country.name.common}`,
+        area: `${country.area}`,
+        continent: `${country.region}`,
+        drivingSide: `${country.car.side}`,
+        flag: `${country.flags.png}`,
+      };
+      countryList.push(infoCountry);
+      countriesGen(infoCountry);
+    }
+  })
+  .catch((error) => console.error("Error:", error));
